fb9759d8aa082d24879910855f738db8adc2bd18
Vijay Kumar Reddy <vijaykumarreddy360@gmail.com>
2026-02-09 22:41:16 +0530

    title changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata = {
-  title: "Construction Company",
+  title: "Aaditya Square Craft Construction Company | House Construction in Bangalore",
   description: "Best construction company in Bangalore",
 };
 
